f04e3525ad1cc6d766c720f91b5c185478757245
Patryk Jaworski <pjaworski.dev@gmail.com>
2022-01-28 08:46:06 +0100

    Add back-to-top & minor changes

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-var brands = document.getElementById("brands_block1");
+// logo scroll, js realisation. Works same as css keyframes, sometimes more buggy
 
-let pos = 0;
-let brandsLoop = setInterval(function(){
-    pos += 0.011;
-    if(pos >= 100) pos = 0;
-    brands.style.transform = `translate3d(${-pos}%, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)`;
-}, 4); 
+// var brands = document.getElementById("brands_block1");
+// var brands2 = document.getElementById("brands_block2");
+
+// let pos = 0;
+// let brandsLoop = setInterval(function(){
+//     pos += 0.011;
+//     if(pos >= 100) pos = 0;
+//     brands.style.transform = `translate3d(${-pos}%, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)`;
+//     brands2.style.transform = `translate3d(${-pos}%, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)`;
+// }, 10); 
+
+
