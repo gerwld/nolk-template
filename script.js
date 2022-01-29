e3ace607f3b7e80885ee1bad4fa3db3ad9025e55
Patryk Jaworski <pjaworski.dev@gmail.com>
2022-01-29 08:26:46 +0100

    Change form step process & ui

--- script.js
+++ script.js
@@ -10,9 +10,15 @@
 //     brands2.style.transform = `translate3d(${-pos}%, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)`;
 // }, 10); 
 
+function validateEmail(email) {
+    var pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return pattern.test(email);
+}
+
 
 // Contact form
 let textarea = document.getElementById('quiz1');
+let button1 = document.getElementById('quiz_btn1');
 let button2 = document.getElementById('quiz_btn2');
 
 let formState = {
@@ -21,22 +27,36 @@ let formState = {
     title: [["Hi!", "What is your name?"],
     ["Nice to meet you", "How can we help you?"],
     ["What is your email?", "So we can reach you."]],
-    placeholder: ["My name is", "Leave us a message", "My email is"]
+    placeholder: ["My name is", "Leave us a message", "My email is"],
+    isSending: false
 }
 
 
-//Enable button if length > 1
 textarea.onkeyup = function () {
-    if(this.value.length > 0) {
+    //Enable button if length > 1
+    if (this.value.length > 0) {
         button2.disabled = false;
     } else button2.disabled = true;
-  };
+
+    //Email check
+    if (formState.step === 2) {
+        if (validateEmail(this.value)) {
+            button2.disabled = false;
+        } else button2.disabled = true;
+    }
+};
 
 function stepClick(isNext) {
     let textBlock = document.getElementById('mainname_text');
+    let fullSection = document.querySelector('.main_name_content');
 
     //save data after click (back/forw)
     formState.userData[formState.step] = textarea.value;
+
+    //Send process
+    if (isNext && formState.step === 2) {
+        formState.isSending = true;
+    }
 
     //inc/decrement step with basic check & enable button
     if (isNext && formState.step < 2) {
@@ -46,9 +66,9 @@ function stepClick(isNext) {
     }
 
     //set data from state & disable button if 0
-    if(formState.step >= 0 && formState.step <= 2) {
+    if (formState.step >= 0 && formState.step <= 2) {
         textarea.value = formState.userData[formState.step];
-        if (formState.userData[formState.step] && formState.userData[formState.step].length > 0){
+        if (formState.userData[formState.step] && formState.userData[formState.step].length > 0) {
             button2.disabled = false;
         } else {
             button2.disabled = true;
@@ -57,38 +77,67 @@ function stepClick(isNext) {
 
     //disable if step 0
     if (formState.step === 0) {
-        document.getElementById('quiz_btn1').disabled = true;
-    } else {
-        document.getElementById('quiz_btn1').disabled = false;
+        button1.disabled = true;
+    } else if (button1) {
+        button1.disabled = false;
     }
 
     let isReversed = (formState.step === 1) && "reversed";
     let isSmileShow = formState.step < 2;
 
-    // Title change
-    textBlock.innerHTML = `
+    if (!formState.isSending) {
+        // Title change
+        textBlock.innerHTML = `
         <div class="jtext_line ${isReversed}">
             ${isSmileShow ? '<div class="lottie-anim lottie-anim__yell"><lottie-player src="img/svg_json/7.json" background="transparent" speed="1" loop autoplay></lottie-player></div>' : ''}
-            <div>${formState.title[formState.step][0]}</div>
+            <div class="quiz_text">${formState.title[formState.step][0]}</div>
         </div>
         <div class="jtext_line">${formState.title[formState.step][1]}</div>
         `;
 
-    //Placeholder change
-    textarea.placeholder = formState.placeholder[formState.step] + "...";
+        //Placeholder change
+        textarea.placeholder = formState.placeholder[formState.step] + "...";
 
-    //Change height if 1
-    if (formState.step == 1) {
-        textarea.rows = 2;
-    } else {
-        textarea.rows = 1;
+        //Change height if 1
+        if (formState.step == 1) {
+            textarea.style.cssText += 'white-space:unset;height:3.5em;';
+            textarea.rows = 3;
+        } else {
+            textarea.style.cssText = '';
+            textarea.rows = 1;
+        }
+
+        //Button change 
+        if (formState.step === 2) {
+            button2.innerHTML = "Submit";
+        } else {
+            button2.innerHTML = "Next";
+        }
     }
 
-    //Button change 
-    if (formState.step === 2) {
-        button2.innerHTML = "Submit";
-    } else {
-        button2.innerHTML = "Next";
+    //Send & show info
+    if (isNext && formState.isSending) {
+        console.log('try to send');
+        if (0) {
+            fullSection.innerHTML = `
+            <div class="mainname_text">
+                <div class="jtext_line">
+                    <div class="lottie-anim lottie-anim__yell"><lottie-player src="img/svg_json/7.json" background="transparent" speed="1" loop autoplay></lottie-player></div>
+                    <div class="quiz_text">Thank you !</div>
+                </div>
+                <div class="jtext_line">
+                    <div class="quiz_text">We’ll get back to you as soon as possible.</div>
+                </div>
+            </div>
+            `;
+        } else {
+            fullSection.innerHTML = `
+                <div class="mainname_text error__quiz">
+                    <div class="jtext_line">Some error occurred.</div>
+                    <button class="quiz_btn quiz_btn__1 btn" id="quiz_btn2" onclick="stepClick(true);">Try again</button>
+                </div>
+            `;
+        }
     }
-  
+
 }
